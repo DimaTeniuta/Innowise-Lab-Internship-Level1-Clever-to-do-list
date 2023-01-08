257a1cc1fb7b0f309dc83a2e8b151a76090fe6ee
dimateniuta <dteniuta@gmail.com>
2023-01-08 14:54:41 +0300

    fix: replace method for ErrorBoundary component

--- src/components/ErrorBoundary/ErrorBoundary.jsx
+++ src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -7,7 +7,7 @@ export default class ErrorBoundary extends Component {
     this.state = { hasError: false };
   }
 
-  componentDidCatch() {
+  getDerivedStateFromError() {
     this.setState({ hasError: true });
   }
 
